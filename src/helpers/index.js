d67cc9772086309ad00f5c90379a3e4d5d0c0d28
dabojimenez <dabojimenez16@hotmail.com>
2024-02-13 19:15:45 -0500

    Calculo de forma d epago

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -10,6 +10,35 @@ const formatearDinero = valor => {
     return formatter.format(valor)
 }
 
+const calcularTotalPagar = (cantidad, plazo) => {
+    let total;
+
+    // Mientras mayor es la cantidad solicitada menor es el interés
+    if (cantidad < 5000) {
+        total = cantidad * 1.5;
+    }else if (cantidad >= 5000 && cantidad < 10000) {
+        total = cantidad * 1.4;
+    }else if (cantidad >= 10000 && cantidad < 15000) {
+        total = cantidad * 1.3;
+    }else{
+        total = cantidad * 1.2;
+    }
+
+    // plazo, a mas plazo mayor interes
+    if (plazo === 3) {
+        total *= 1.1;
+    }else if (plazo === 6) {
+        total *= 1.2;
+    }else if (plazo === 12) {
+        total *= 1.3;
+    }else{
+        total *= 1.4;
+    }
+
+    return total;
+}
+
 export {
     formatearDinero,
+    calcularTotalPagar,
 }
